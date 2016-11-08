48e8ab49ff7eab8939f9660675b85332939a7659
Jonathan Siegel <jwsiegel@ucla.edu>
2016-11-07 16:23:11 -0800

    Added model tester.

---
+++ model_tester.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+# Tests a model by compressing the Hitch Hiker's Guide to the Galaxy,
+# calculating the compression ratio and checking to be sure that the decompressed
+# text is identical to the original.
+
+from source_coder import compress
+from source_coder import decompress
+from trivial_model import TrivialModel # change this line to test a different model.
+
+model = TrivialModel() # change this line to test a different model.
+
+message = ''
+with open('HitchHikersGuide.txt','r') as myfile:
+	message = myfile.read()
+
+compressed_message = compress(message, model)
+print("The compression ratio (over ascii character encoding) is:", 2141320.0/(len(compressed_message[1]) + 32))
+recovered_message = decompress(compressed_message, model)
+if (recovered_message == message):
+	print("Recovery: SUCCESS")
+else:
+	print("Recovery: FAILURE")
